568f68c19fbd38d1852ce40fc66a2d1d7444a553
Jaspar <j.cyxtus@gmail.com>
2021-03-19 18:16:38 +0000

    Todo 1: Command Line Arguments
    Added retrieval of command line inputs

--- check_images.py
+++ check_images.py
@@ -2,9 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/check_images.py
 #
-# TODO 0: Add your information below for Programmer & Date Created.                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                  
+# # PROGRAMMER: Ifesinachi Chukwuemeka
+# DATE CREATED: 19/03/2021                   
 # REVISED DATE: 
 # PURPOSE: Classifies pet images using a pretrained CNN model, compares these
 #          classifications to the true identity of the pets in the images, and
@@ -39,17 +38,10 @@ from print_results import print_results
 
 # Main program function defined below
 def main():
-    # TODO 0: Measures total program runtime by collecting start time
     start_time = time()
     
-    # TODO 1: Define get_input_args function within the file get_input_args.py
-    # This function retrieves 3 Command Line Arugments from user as input from
-    # the user running the program from a terminal window. This function returns
-    # the collection of these command line arguments from the function call as
-    # the variable in_arg
     in_arg = get_input_args()
 
-    # Function that checks command line arguments using in_arg  
     check_command_line_arguments(in_arg)
 
     
@@ -115,10 +107,8 @@ def main():
     # and incorrectly classified breeds (if requested)
     print_results(results, results_stats, None, True, True)
     
-    # TODO 0: Measure total program runtime by collecting end time
     end_time = time()
     
-    # TODO 0: Computes overall runtime in seconds & prints it in hh:mm:ss format
     tot_time = end_time - start_time
     print("\n** Total Elapsed Runtime:",
         str(int((tot_time/3600)))+":"+str(int((tot_time%3600)/60))+":"
--- get_input_args.py
+++ get_input_args.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_input_args.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                   
+# PROGRAMMER: Ifesinachi Chukwuemeka
+# DATE CREATED: 19/03/2021                        
 # REVISED DATE: 
 # PURPOSE: Create a function that retrieves the following 3 command line inputs 
 #          from the user using the Argparse Python module. If the user fails to 
@@ -17,17 +17,8 @@
 # Imports python modules
 import argparse
 
-# TODO 1: Define get_input_args function below please be certain to replace None
-#       in the return statement with parser.parse_args() parsed argument 
-#       collection that you created with this function
-# 
 def get_input_args():
     """
-    Retrieves and parses the 3 command line arguments provided by the user when
-    they run the program from a terminal window. This function uses Python's 
-    argparse module to created and defined these 3 command line arguments. If 
-    the user fails to provide some or all of the 3 arguments, then the default 
-    values are used for the missing arguments. 
     Command Line Arguments:
       1. Image Folder as --dir with default value 'pet_images'
       2. CNN Model Architecture as --arch with default value 'vgg'
@@ -38,6 +29,10 @@ def get_input_args():
     Returns:
       parse_args() -data structure that stores the command line arguments object  
     """
-    # Replace None with parser.parse_args() parsed argument collection that 
-    # you created with this function 
-    return None
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--dir', default='pet_images/', type=str, help='Enter path to the images folder "pet_images/"')
+    parser.add_argument('--arch', default='vgg', type=str, help='Choose CNN model to use. "vgg"')
+    parser.add_argument('--dogfile', default='dognames.txt', type=str, help='Enter name of text file containing list of dog names. "dognames.txt"')
+
+    return parser.parse_args()
