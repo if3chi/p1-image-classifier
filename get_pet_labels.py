f3c3eedf6068e51c0137be38ed31d10a29f32d77
Jaspar <j.cyxtus@gmail.com>
2021-03-19 18:24:46 +0000

    Todo 2: Pet Labels
    Created a dictionary of pet labels

--- get_pet_labels.py
+++ get_pet_labels.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                  
+# PROGRAMMER: Ifesinachi Chukwuemeka
+# DATE CREATED: 19/03/2021             
 # REVISED DATE: 
 # PURPOSE: Create the function get_pet_labels that creates the pet labels from 
 #          the image's filename. This function inputs: 
@@ -19,27 +19,33 @@
 # Imports python modules
 from os import listdir
 
-# TODO 2: Define get_pet_labels function below please be certain to replace None
-#       in the return statement with results_dic dictionary that you create 
-#       with this function
-# 
 def get_pet_labels(image_dir):
-    """
-    Creates a dictionary of pet labels (results_dic) based upon the filenames 
-    of the image files. These pet image labels are used to check the accuracy 
-    of the labels that are returned by the classifier function, since the 
-    filenames of the images contain the true identity of the pet in the image.
-    Be sure to format the pet labels so that they are in all lower case letters
-    and with leading and trailing whitespace characters stripped from them.
-    (ex. filename = 'Boston_terrier_02259.jpg' Pet label = 'boston terrier')
+	"""
     Parameters:
-     image_dir - The (full) path to the folder of images that are to be
-                 classified by the classifier function (string)
+    image_dir - The (full) path to the folder of images that are to be
+                classified by the classifier function (string)
     Returns:
-      results_dic - Dictionary with 'key' as image filename and 'value' as a 
-      List. The list contains for following item:
-         index 0 = pet image label (string)
+        results_dic - Dictionary with 'key' as image filename and 'value' as a 
+        List. The list contains for following item:
+        index 0 = pet image label (string)
     """
-    # Replace None with the results_dic dictionary that you created with this
-    # function
-    return None
+	
+	results_dic = dict()
+	filenames_list = listdir(image_dir)
+
+	for filename in filenames_list:
+		pet_label = ''
+
+		for name in filename.lower().split('_'):
+			if name.isalpha():
+				pet_label += name + ' '
+			
+		if filename not in results_dic:
+			results_dic[filename] = pet_label.strip()
+		else:
+			print("** Warning: Key=", filename, 
+				"already exists in results_dic with value =",
+				results_dic[filename])
+
+	return results_dic
+
